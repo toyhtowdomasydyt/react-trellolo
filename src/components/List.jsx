e9d7517042c90046f36c6bc3ce34c6d1bf454299
Joel D <imaginethere.snoie@gmail.com>
2021-08-22 15:28:01 +0300

    Add logic for card editing

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 import {useSelector} from "react-redux";
 import "./List.css";
 
 import Card from "./Card";
+import shortid from "shortid";
+import CardEditor from "./CardEditor";
 
-const List = ({listId, index}) => {
+const List = ({listId, index, dispatch}) => {
     const listState = useSelector(state => state.listsById[listId]);
+    const [addingCard, setAddingCard] = useState(false);
+
+    const toggleAddingCard = () => setAddingCard(!addingCard);
+
+    const addCard = async cardText => {
+        toggleAddingCard();
+
+        const cardId = shortid.generate();
+
+        dispatch({
+            type: "ADD_CARD",
+            payload: { cardText, cardId, listId }
+        });
+    };
+
 
     return (
         <div className="List">
@@ -22,6 +39,18 @@ const List = ({listId, index}) => {
                         listId={listState.id}
                     />
                 ))}
+
+                {addingCard ? (
+                    <CardEditor
+                        onSave={addCard}
+                        onCancel={toggleAddingCard}
+                        adding
+                    />
+                ) : (
+                    <div className="Toggle-Add-Card" onClick={toggleAddingCard}>
+                        <ion-icon name="add" /> Add a card
+                    </div>
+                )}
             </div>
         </div>
     );
